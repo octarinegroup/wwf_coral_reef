13d3d3ff6a8f57d20900b82c696d316ff8d011e3
derekbwarren <derekbwarren@gmail.com>
2018-04-29 16:49:08 -0400

    updated map container

--- frontend/src/Containers/MapContainer.js
+++ frontend/src/Containers/MapContainer.js
@@ -16,32 +16,38 @@ import Sidebar from '../Components/Sidebar/Sidebar'
 import DataTable from '../Components/DataTable/DataTable'
 import Graph from '../Components/Graph/Graph'
 import './MapContainer.css'
-import BenthicTimeSeriesGraph from '../Components/BenthicTimeSeriesGraph/BenthicTimeSeriesGraph';
+import BenthicTimeSeriesGraph from '../Components/BenthicTimeSeriesGraph/BenthicTimeSeriesGraph'
+import { SidebarCollapsed } from '../Components/Sidebar/SidebarCollapsed'
+import { GraphsSidebar } from '../Components/GraphsSidebar/GraphsSidebar'
 
 class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        benthicData: null,
-				isLoaded: false,
-				coverType: 'PercentCoral',
-        filterYear: 2016,
-        SEType: 'SECoral',
-        isDataView: true,
-        isFiltered: false,
-        filteredData: null
+      benthicData: null,
+      isLoaded: false,
+      coverType: 'PercentCoral',
+      filterYear: 2016,
+      SEType: 'SECoral',
+      isDataView: true,
+      isFiltered: false,
+      filteredData: null,
+      sidebarCollapsed: false,
+      showDataView: false
     };
     this.handleCoverTypeChange = this.handleCoverTypeChange.bind(this)
     this.handleFilterYearChange = this.handleFilterYearChange.bind(this)
     this.toggleDataGraph = this.toggleDataGraph.bind(this)
     this.filterRawData = this.filterRawData.bind(this)
     this.removeDataFilter = this.removeDataFilter.bind(this)
+    this.toggleSidebar = this.toggleSidebar.bind(this)
+    this.toggleShowData = this.toggleShowData.bind(this)
   }
 
   handleCoverTypeChange(e) {
     let SEType = this.state.coverType;
 
-    switch(e.target.value) {
+    switch (e.target.value) {
       case 'PercentCoral':
         SEType = 'SECoral'
         break;
@@ -90,46 +96,101 @@ class MapContainer extends Component {
       filterYear: Number.parseInt(filterYear)
     })
   }
- 
+
   toggleDataGraph() {
     this.setState({
       isDataView: !this.state.isDataView
     })
   }
 
-  componentDidMount() {
-    getBenthicData()
-    .then(benthicData => {
-        this.setState({
-            benthicData: benthicData.data.data,
-            filteredData: benthicData.data.data,
-            isLoaded: true
-        })
+  toggleSidebar() {
+    this.setState({
+      sidebarCollapsed: !this.state.sidebarCollapsed
     })
   }
 
+  toggleShowData() {
+    this.setState({
+      showDataView: !this.state.showDataView
+    }, () => {
+      if (!this.state.sidebarCollapsed) {
+        this.toggleSidebar()
+      } else if(!this.state.showDataView) {
+        this.toggleSidebar()
+      }
+    })
+  }
+
+  componentDidMount() {
+    getBenthicData()
+      .then(benthicData => {
+        this.setState({
+          benthicData: benthicData.data.data,
+          filteredData: benthicData.data.data,
+          isLoaded: true
+        })
+      })
+  }
+
   render() {
-  
-    return (
-      this.state.isLoaded ? 
-      <div>
-        <div className="map-container">
-          <Sidebar toggleDataGraph={this.toggleDataGraph} isDataView={this.state.isDataView} coverType={this.state.coverType} filterYear={this.state.filterYear} handleCoverTypeChange={this.handleCoverTypeChange} handleFilterYearChange={this.handleFilterYearChange}/>
-          <BenthicMap filterRawData={this.filterRawData} removeDataFilter={this.removeDataFilter} benthicData={this.state.benthicData} coverType={this.state.coverType} filterYear={this.state.filterYear} />
-        </div>
-        <div className='data-container'>
-          { this.state.isDataView ?
-            <DataTable data={this.state.filteredData} coverType={this.state.coverType} filterYear={this.state.filterYear} isFiltered={this.state.isFiltered}/>
-            :
-            <div className="graph-container">
-              <Graph data={this.state.filteredData} coverType={this.state.coverType} isFiltered={this.state.isFiltered} SEType={this.state.SEType} filterYear={this.state.filterYear} />
-              <BenthicTimeSeriesGraph data={this.state.benthicData} coverType={this.state.coverType} isFiltered={this.state.isFiltered} SEType={this.state.SEType} filterYear={this.state.filterYear}/>
-            </div>
-          }
-        </div>
-      </div>
+    const dataView = this.state.isDataView ?
+      <DataTable data={this.state.filteredData} coverType={this.state.coverType} filterYear={this.state.filterYear} isFiltered={this.state.isFiltered} />
       :
-      null
+      <div className="graph-container">
+        <Graph data={this.state.filteredData} coverType={this.state.coverType} isFiltered={this.state.isFiltered} SEType={this.state.SEType} filterYear={this.state.filterYear} />
+        <BenthicTimeSeriesGraph data={this.state.benthicData} coverType={this.state.coverType} isFiltered={this.state.isFiltered} SEType={this.state.SEType} filterYear={this.state.filterYear} />
+      </div>
+    return (
+      this.state.isLoaded ?
+        <div className="map-container-wrapper">
+          {
+            this.state.sidebarCollapsed ?
+              <SidebarCollapsed toggleSidebar={this.toggleSidebar} />
+              : <Sidebar
+                toggleDataGraph={this.toggleDataGraph}
+                isDataView={this.state.isDataView}
+                coverType={this.state.coverType}
+                filterYear={this.state.filterYear}
+                handleCoverTypeChange={this.handleCoverTypeChange}
+                handleFilterYearChange={this.handleFilterYearChange}
+                toggleSidebar={this.toggleSidebar}
+                showDataView={this.state.showDataView}
+              />
+          }
+          <BenthicMap
+            filterRawData={this.filterRawData}
+            removeDataFilter={this.removeDataFilter}
+            benthicData={this.state.benthicData}
+            coverType={this.state.coverType}
+            filterYear={this.state.filterYear}
+          />
+          <GraphsSidebar showDataView={this.state.showDataView} />
+          <div className='data-container'>
+            {
+              this.state.showDataView ?
+                dataView
+                : null
+            }
+            <div
+              className={`data-view-show ${this.state.showDataView ? 'top-right' : ''}`}
+              onClick={this.toggleShowData}
+            >
+              {this.state.showDataView ? 'Hide' : 'Show'} Data
+            </div>
+            {
+              this.state.showDataView ?
+                <div
+                  className={`data-view-show ${this.state.showDataView ? 'top-left' : ''}`}
+                  onClick={this.toggleDataGraph}
+                >
+                  Show {this.state.isDataView ? 'Graph' : 'Table'}
+                </div>
+                : null
+            }
+          </div>
+        </div>
+        :
+        null
     );
   }
 }
