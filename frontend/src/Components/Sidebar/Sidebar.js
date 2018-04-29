91c47fd32c4d0f74f305ed347c2b4da702aba7b9
derekbwarren <derekbwarren@gmail.com>
2018-04-29 16:50:11 -0400

    added back arrow to collapse sidebar

--- frontend/src/Components/Sidebar/Sidebar.js
+++ frontend/src/Components/Sidebar/Sidebar.js
@@ -7,77 +7,82 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './Sidebar.css'
 
 class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        year: props.filterYear,
-        yearSlider: props.filterYear
+    constructor(props) {
+        super(props);
+        this.state = {
+            year: props.filterYear,
+            yearSlider: props.filterYear
+        }
+        this.changeYear = this.changeYear.bind(this)
     }
-    this.changeYear = this.changeYear.bind(this)
-  }
 
-  changeYear(event, value) {
-    this.setState({
-        yearSlider: value
-    })
-    this.props.handleFilterYearChange(value)
-  }
-  render() {
-    return (
-      <div className="sidebar-display">
-            <form>
-                <h5> Cover Type </h5>
-                <div>
-                    <input className="with-gap" type='radio' value='PercentCoral'
-                    checked={this.props.coverType === 'PercentCoral'} id='cover-type-1'
-                    onClick={(e) => this.props.handleCoverTypeChange(e)}/>
-                    <label htmlFor='cover-type-1'> Percent Coral </label>
+    changeYear(event, value) {
+        this.setState({
+            yearSlider: value
+        })
+        this.props.handleFilterYearChange(value)
+    }
+    render() {
+        return (
+            <div className={`sidebar-display ${this.props.showDataView ? 'bg-for-data-sidebar' : ''}`}>
+                <div className="sidebar-arrow" onClick={this.props.toggleSidebar}>
+                    <i className="material-icons">arrow_backward</i>
                 </div>
-                <div>
-                    <input className="with-gap" type='radio' value='PercentSoftCoral'
-                    checked={this.props.coverType === 'PercentSoftCoral'} id='cover-type-2'
-                    onClick={(e) => this.props.handleCoverTypeChange(e)}/>
-                    <label htmlFor='cover-type-2'> Percent Soft Coral </label>
+                <form>
+                    <h5> Cover Type </h5>
+                    <div>
+                        <input className="with-gap" type='radio' value='PercentCoral'
+                            checked={this.props.coverType === 'PercentCoral'} id='cover-type-1'
+                            onClick={(e) => this.props.handleCoverTypeChange(e)} />
+                        <label htmlFor='cover-type-1'> Percent Coral </label>
+                    </div>
+                    <div>
+                        <input className="with-gap" type='radio' value='PercentSoftCoral'
+                            checked={this.props.coverType === 'PercentSoftCoral'} id='cover-type-2'
+                            onClick={(e) => this.props.handleCoverTypeChange(e)} />
+                        <label htmlFor='cover-type-2'> Percent Soft Coral </label>
+                    </div>
+                    <div>
+                        <input className="with-gap" type='radio' value='PercentCCA'
+                            checked={this.props.coverType === 'PercentCCA'} id='cover-type-3'
+                            onClick={(e) => this.props.handleCoverTypeChange(e)} />
+                        <label htmlFor='cover-type-3'> Percent CCA </label>
+                    </div>
+                    <div>
+                        <input className="with-gap" type='radio' value='PercentBleached'
+                            checked={this.props.coverType === 'PercentBleached'} id='cover-type-4'
+                            onClick={(e) => this.props.handleCoverTypeChange(e)} />
+                        <label htmlFor='cover-type-4'>Percent Bleached</label>
+                    </div>
+                    <div>
+                        <input className="with-gap" type='radio' value='PercentRubble'
+                            checked={this.props.coverType === 'PercentRubble'} id='cover-type-5'
+                            onClick={(e) => this.props.handleCoverTypeChange(e)} />
+                        <label htmlFor='cover-type-5'>Percent Rubble </label>
+                    </div>
+                    <div>
+                        <input className="with-gap" type='radio' value='PercentOthAlgae'
+                            checked={this.props.coverType === 'PercentOthAlgae'} id='cover-type-6'
+                            onClick={(e) => this.props.handleCoverTypeChange(e)} />
+                        <label htmlFor='cover-type-6'> Percent Other Algae </label>
+                    </div>
+                </form>
+                <div className="sidebar-filter-wrapper">
+                    <h5>Filtered Year:</h5>
+                    <p> {this.state.yearSlider} </p>
+                    <MuiThemeProvider>
+                        <Slider
+                            min={2000}
+                            max={2018}
+                            step={1}
+                            value={this.state.yearSlider}
+                            onChange={this.changeYear} 
+                        />
+                    </MuiThemeProvider>
                 </div>
-                <div>
-                    <input className="with-gap" type='radio' value='PercentCCA'
-                    checked={this.props.coverType === 'PercentCCA'} id='cover-type-3'
-                    onClick={(e) => this.props.handleCoverTypeChange(e)}/>
-                    <label htmlFor='cover-type-3'> Percent CCA </label>
-                </div>
-                <div>
-                    <input className="with-gap" type='radio' value='PercentBleached'
-                    checked={this.props.coverType === 'PercentBleached'} id='cover-type-4'
-                    onClick={(e) => this.props.handleCoverTypeChange(e)}/>
-                     <label htmlFor='cover-type-4'>Percent Bleached</label>
-                </div>
-                <div>
-                    <input className="with-gap" type='radio' value='PercentRubble'
-                    checked={this.props.coverType === 'PercentRubble'} id='cover-type-5'
-                    onClick={(e) => this.props.handleCoverTypeChange(e)}/>
-                    <label htmlFor='cover-type-5'>Percent Rubble </label>
-                </div>
-                <div>
-                    <input className="with-gap" type='radio' value='PercentOthAlgae'
-                    checked={this.props.coverType === 'PercentOthAlgae'} id='cover-type-6'
-                    onClick={(e) => this.props.handleCoverTypeChange(e)}/>
-                    <label htmlFor='cover-type-6'> Percent Other Algae </label>
-                </div>
-            </form>
-            <h5> Filtered Year: </h5>
-            <p> {this.state.yearSlider} </p>
-            <MuiThemeProvider>
-                <Slider 
-                    min={2000}
-                    max={2018}
-                    step={1}
-                    value={this.state.yearSlider} 
-                    onChange={this.changeYear} />
-            </MuiThemeProvider>
-            <Button className='center-submit' waves='light' onClick={this.props.toggleDataGraph}>{this.props.isDataView ? 'View Graph' : 'View Table'}</Button>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default Sidebar
